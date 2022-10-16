36dd66182019f497f95ed51357446b71e2a56be0
HaAbdullah <101064862+HaAbdullah@users.noreply.github.com>
2022-10-16 19:27:20 -0400

    Solved the bug where you have to reload page to type in code
    basic gui implemented.

--- content.js
+++ content.js
@@ -1,13 +1,16 @@
 chrome.runtime.onConnect.addListener(function(port){});3
 
 var code = "";
+start();
 
+function start() {
 window.onkeydown = function(event) {
     if (event.key == "`") {  
-        getKeys()
+        code = "";
+        getKeys()   
     } 
 }
-
+}
 
 function getKeys() {
     window.onkeydown = function(event) {
@@ -15,14 +18,16 @@ function getKeys() {
             pressedEnter() 
         } else if (event.key.length == 1){
             code = code + event.key
-        }   
+        } 
     }
+
 }
 
 const socialsList = ["https://www.instagram.com/", "https://discord.com/channels/@me/", "https://twitter.com/?lang=en"];
 const gamesList = ["https://addictinggames.com"];
 const videoList = ["https://youtube.com", "https://www.twitch.tv/"];
 const streamingList = ["https://www.amazon.com/Prime-Video/b?node=2676882011", "https://www.netflix.com/browse", "https://www.disneyplus.com/login"]
+
 
 function pressedEnter(){ // future: parse code into window.open to compress code i
     // Called when enter key is pressed any time after the secret key 
@@ -33,28 +38,33 @@ function pressedEnter(){ // future: parse code into window.open to compress code
         while (i < socialsList.length) {
             window.open(socialsList[i]);
             i++;
+            start();
         }
     } 
     else if (code == "games"){
         while (i < gamesList.length) {
             window.open(gamesList[i]);
             i++;
+            start();
         } 
     }
     else if (code == "video"){
         while (i < videoList.length) {
             window.open(videoList[i]);
             i++;
+            start();
         } 
     }
     else if (code == "streaming"){
         while (i < streamingList.length) {
             window.open(streamingList[i]);
             i++;
+            start();
         } 
     }
     else{
         alert("Invalid Code: " + code);
+        start();
     }
 }
 
